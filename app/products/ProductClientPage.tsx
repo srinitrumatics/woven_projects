18ecf9c8d61707808bcb1af8b057ad248ee0ff34
mathu kasi <mathu@trumatics.com>
2026-05-22 11:14:53 +0530

    uipdated code

--- app/products/ProductClientPage.tsx
+++ app/products/ProductClientPage.tsx
@@ -450,7 +450,7 @@ function Content() {
 }
 
 export default function ProductClientPage() {
-  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || "dev_woven_products";
+  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || "wovn_products_local";
 
   if (!process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || !process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY) {
     return (
