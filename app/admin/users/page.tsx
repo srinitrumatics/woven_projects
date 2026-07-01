73dbb4d26ff9dc6ade5b0ca7655dbd59d0464825
mathu kasi <mathu@trumatics.com>
2026-07-01 22:51:35 +0530

    updated contact page

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -292,9 +292,9 @@ const UserManagement: React.FC = () => {
   return (
     <ProtectedRoute>
       <div className="mb-6">
-        <h1 className="text-3xl font-bold text-gray-900 dark:text-white">User Management</h1>
+        <h1 className="text-3xl font-bold text-gray-900 dark:text-white">Account Contacts</h1>
         <p className="text-gray-600 dark:text-gray-400 text-[16px] mt-1 truncate" title="Manage contacts assigned to your company account">
-          Manage contacts assigned to your company account
+          Account contacts related to your company
         </p>
       </div>
 
